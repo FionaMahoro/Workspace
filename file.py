de09113b7549a029ed308fdbf035381eaedf0bfa
Fiona <fionamahoro99@gmail.com>
2023-11-10 16:01:16 +0200

    first commit

---
+++ file.py
@@ -0,0 +1,12 @@
+# P_Name ="john Smith"
+# P_age =20
+# P_admit="New"
+# print (P_Name ,P_age, P_admit)
+
+
+# first_number= input('Enter number: ')
+# Second_number= input('Enter number: ')
+# sum = int(first_number) + int(Second_number) 
+# # Python can only concatnate str
+# print ("The sum is " + str(sum))
+
